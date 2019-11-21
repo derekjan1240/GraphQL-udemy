399d7a5a36a1201ece4897e9727df9d6804326da
Derek Zhan <derekjan1240@gmail.com>
2019-11-22 05:23:30 +0800

    finished section 3

---
+++ graphQL-basic/src/resolvees/Mutation.js
@@ -0,0 +1,202 @@
+import uuid4 from 'uuid/v4'
+
+const Mutation = {
+    createUser(parent, args,{ db }, info){
+        const emailTaken = db.users.some((user)=>{
+            return user.email === args.data.email
+        })
+
+        if (emailTaken){
+            throw new Error('Email taken.')
+        }
+
+        const user = {
+            id: uuid4(),
+            ...args.data
+        }
+
+        db.users.push(user)
+
+        return user
+    },
+    deleteUser(parent, args, { db }, info){
+        const userIndex = db.users.findIndex((user)=>{
+            return user.id === args.id
+        })
+
+        if(userIndex === -1){
+           throw new Error('User not found.') 
+        }
+
+        const deleteUsers = db.users.splice(userIndex, 1)
+
+        // delete post and post comment
+        db.posts = db.posts.filter((post)=>{
+            const match = post.author === args.id
+
+            if(match){
+                db.comments = db.comments.filter((comment)=>{
+                    return comment.post !== post.id
+                })
+            }
+
+            return !match
+        })
+
+        // delete comments by user
+        db.comments = db.comments.filter((comment)=>{
+            return comment.author !== args.id
+        })
+
+        return deleteUsers[0]
+    },
+    updateUser(parent, args, { db }, info){
+        const { id, data } = args
+        const user = db.users.find((user)=>{
+            return user.id === id
+        })
+
+        if(!user){
+            throw new Error('User not found')
+        }
+
+        if ( typeof data.email === 'string'){
+            const emailTaken = db.users.some((user)=>{
+                return user.email === data.email
+            })
+    
+            if (emailTaken){
+                throw new Error('Email taken.')
+            }
+
+            user.email = data.email
+        }
+
+        if( typeof data.name === 'string'){
+            user.name = data.name
+        }
+
+        if( typeof data.age != 'undefined'){
+            user.age = data.age
+        }
+
+        return user         
+    },
+    createPost(parent, args, { db }, info){
+        const userExists = db.users.some((user)=>{
+            return user.id === args.data.author
+        })
+
+        if(!userExists){
+            throw new Error('User not found')
+        }
+
+        const post = {
+            id: uuid4(),
+            ...args.data
+        }
+
+        db.posts.push(post);
+
+        return post
+    },
+    updatePost(parent, args, { db }, info){
+        const { id, data } = args
+        const post = db.posts.find((post)=>{
+            return post.id === id
+        })
+
+        if(!post){
+            throw new Error('Post not found')
+        }
+
+        if ( typeof data.title === 'string'){
+            post.title = data.title
+        }
+
+        if ( typeof data.body === 'string'){
+            post.body = data.body
+        }
+
+        if ( typeof data.published === 'boolean'){
+            post.published = data.published
+        }
+
+        return post         
+    },
+    deletePost(parent, args, { db }, info){
+        const postIndex = db.posts.findIndex((post)=>{
+            return post.id === args.id
+        })
+
+        if(postIndex === -1){
+            throw new Error('Post not found')
+        }
+
+        const deletePosts = db.posts.splice(postIndex, 1)
+
+        db.comments = db.comments.filter((comment)=>{
+            return comment.post !== args.id
+        })
+
+        return deletePosts[0]
+
+    }, 
+    createComment(parent, args, { db }, info){
+        const userExists = db.users.some((user)=>{
+            return user.id === args.data.author
+        })
+
+        const PostExists = db.posts.some((post)=>{
+            return (post.id === args.data.post && post.published)
+        })
+        
+        if(!userExists){
+            throw new Error('User not found')
+        }
+
+        if(!PostExists){
+            throw new Error('Post not found')
+        }
+
+        const comment = {
+            id: uuid4(),
+            ...args.data
+        }
+
+        db.comments.push(comment);
+
+        return comment
+    },
+    deleteComment(parent, args, { db }, info){
+        const commentIndex = db.comments.findIndex((comment)=>{
+            return comment.id === args.id
+        })
+
+        if(commentIndex === -1){
+            throw new Error('Comment not found')
+        }
+
+        const deleteComments = db.comments.splice(commentIndex, 1)
+
+        return deleteComments[0]
+    },
+    updateComment(parent, args, { db }, info){
+        const { id, data } = args
+        const comment = db.comments.find((comment)=>{
+            return comment.id === id
+        })
+
+        if(!comment){
+            throw new Error('Comment not found')
+        }
+
+        if ( typeof data.text === 'string'){
+            comment.text = data.text
+        }
+
+        return comment      
+    }
+}
+
+export { Mutation as default}
